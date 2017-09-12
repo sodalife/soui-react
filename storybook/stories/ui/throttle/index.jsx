93a5f289d26efb0b0b6746f7fdb84ba0471bc6fc
yelo <zhihuzeye@gmail.com>
2017-09-12 21:32:09 +0800

    update Throttle story

--- storybook/stories/ui/throttle/index.jsx
+++ storybook/stories/ui/throttle/index.jsx
@@ -15,6 +15,22 @@ const stories = storiesOf('节流阀 Throttle (实验性)', module)
 stories.addDecorator(withKnobs)
 
 stories.add('基础', withInfo()(() => {
+  let props = {
+    waitText: ({ countdown }) => `重获验证码 (${countdown})`,
+    children: text('children', '获取验证码'),
+    onClick: action('click'),
+    onTick: action('tick'),
+    startedAt: number('startedAt', +new Date()),
+    wait: number('wait', 10 * 1000),
+    disabled: boolean('disabled', false),
+  }
+  return (<div className={cx(styles.phone, styles.center)}>
+    <Throttle className={styles.send} disabled={false} {...props} />
+    <p className={styles.warning}>提醒：Notice 组件仍处实验性状态，后续版本 API 可能发生变化。</p>
+  </div>)
+}))
+
+stories.add('完整示例', withInfo()(() => {
   class Story extends Component {
     constructor () {
       super()
@@ -24,15 +40,14 @@ stories.add('基础', withInfo()(() => {
       let props = {
         waitText: ({ countdown }) => `重获验证码 (${countdown})`,
         children: text('children', '获取验证码'),
-        onClick: () => this.setState({ startedAt: +new Date() }, () => action('click')),
-        onTick: (countdown) => action('tick', countdown),
+        onClick: () => this.setState({ startedAt: +new Date() }, action('click')),
+        onTick: action('tick'),
         startedAt: this.state.startedAt,
         wait: number('wait', 10 * 1000),
         disabled: boolean('disabled', false),
       }
       return (<div className={cx(styles.phone, styles.center)}>
         <Throttle className={styles.send} disabled={false} {...props} />
-        <p className={styles.warning}>提醒：Notice 组件仍处实验性状态，后续版本 API 可能发生变化。</p>
       </div>)
     }
   }
