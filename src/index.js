ea5d09f97e6b9172bb90eed886240c3f82a530e2
yelo <zhihuzeye@gmail.com>
2017-09-14 20:40:53 +0800

    add Dialog

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ export { default as Tag } from './components/tag/index.jsx'
 export { default as Jumbotron } from './components/jumbotron/index.jsx'
 export { default as Navbar } from './components/navbar/index.jsx'
 
+export { default as Dialog } from './dialogs/dialog/index.jsx'
+
 export { default as Throttle } from './helpers/throttle/index.jsx'
 export { default as Fastscroll } from './helpers/fastscroll/index.jsx'
 export { default as Clearfix } from './helpers/clearfix/index.jsx'
