f11cb14c371af36a60f8eea72cd0eea8cac17f08
yelo <zhihuzeye@gmail.com>
2017-09-15 20:00:13 +0800

    update alert and ghostify logic

---
+++ src/dialogs/modals/ghosts/alert.jsx
@@ -0,0 +1,4 @@
+import Alert from '../alert/index.jsx'
+import ghostify from './ghostify/index.jsx'
+
+export default ghostify(Alert)
--- storybook/stories/dialogs/alert/index.jsx
+++ storybook/stories/dialogs/alert/index.jsx
@@ -6,11 +6,11 @@ import { action } from '@storybook/addon-actions'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 
-import { Alert } from '../../../../dist/soui'
+import { Alert, alert, Button } from '../../../../dist/soui'
 
 import styles from './style.pcss'
 
-const stories = storiesOf('对话 / 警告 Alert', module)
+const stories = storiesOf('对话 / 情景 / 警告 Alert', module)
 
 stories.addDecorator(withKnobs)
 
@@ -27,5 +27,30 @@ stories.add('基础', withInfo()(() => {
 
   return (<div className={styles.phone}>
     <Alert {...props} />
+  </div>)
+}))
+
+stories.add('幽灵模式', withInfo()(() => {
+  let props = {
+    title: text('title', '标题'),
+    message: text('message', '内容'),
+    okText: text('okText', 'OK'),
+    okDisabled: boolean('okDisabled', false),
+  }
+  return (<div className={styles.phone}>
+    <Button onClick={() => alert(props)} type="ghost">警报</Button>
+  </div>)
+}))
+
+stories.add('幽灵模式 - 延迟关闭', withInfo()(() => {
+  function onOk () {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve()
+      }, 1800)
+    })
+  }
+  return (<div className={styles.phone}>
+    <Button onClick={() => alert({ message: 'message', onOk })} type="ghost">警报</Button>
   </div>)
 }))
