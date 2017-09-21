1a8945bac034ff806c62961958936a467773390c
yelo <zhihuzeye@gmail.com>
2017-09-21 22:24:35 +0800

    update sorting of storybook

--- storybook/config.js
+++ storybook/config.js
@@ -12,22 +12,22 @@ function loadStories () {
   require('./stories/basics/image')
   require('./stories/basics/view')
 
+  require('./stories/components/input')
   require('./stories/components/list')
-  require('./stories/components/notice')
   require('./stories/components/panel')
   require('./stories/components/tag')
   require('./stories/components/jumbotron')
   require('./stories/components/navbar')
-  require('./stories/components/input')
+  require('./stories/components/notice')
 
   require('./stories/dialogs/dialog')
   require('./stories/dialogs/modals/alert')
   require('./stories/dialogs/modals/confirm')
   require('./stories/dialogs/modals/prompt')
 
-  require('./stories/helpers/throttle')
   require('./stories/helpers/fastscroll')
   require('./stories/helpers/clearfix')
+  require('./stories/helpers/throttle')
 }
 
 configure(loadStories, module)
